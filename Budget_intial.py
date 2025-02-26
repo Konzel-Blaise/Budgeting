27b54507a47b6297596d470497411fe0569ae891
Konzel-Blaise <69052202+Konzel-Blaise@users.noreply.github.com>
2025-02-25 18:42:09 -0800

    Add files via upload

---
+++ Budget_intial.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+#trying to automate budgeting from capital one csv exported statements. 
+
+# import numpy as np
+# import csv
+import pandas as pd
+# import os
+import matplotlib.pyplot as plt
+from func_load_cap1 import *
+
+#import new data from downlaods 
+path = "/Users/blazer/Downloads"
+search_string = 'transaction_download'
+
+dataframes = load_expense_history(path, search_string)
+
+print("NEW DATA IMPORTED....PLEASE REVIEW")
+print(dataframes)
+
+
+#%% section to merge with existing data
+
+#import MASTER data from working directory 
+
+MASTER = load_expense_history("/Users/blazer/PyProjects/Personal_Finance/Budgeting", "MASTER_SPEND_HISTORY")
+MASTER = MASTER[0]
+print(MASTER)
+
+# Append or concatenate all DataFrames in the list
+RECENT = pd.concat(dataframes, ignore_index=True)
+# print(f'{RECENT}')
+
+# this was used to write original file - probably wont need again
+#RECENT.to_csv('MASTER_SPEND_HISTORY.csv', index=False)  
+
+#%% Update MASTER 
+
+MASTER = pd.concat([MASTER, RECENT], ignore_index=True)
+MASTER.to_csv('MASTER_SPEND_HISTORY.csv', index=False)  
+
+#%%
+
+#remove credit payments marked as NaN in the Debit column
+credit_expenses = RECENT.dropna(subset=['Debit'])
+
+#calc number totals of each categories in fallout
+merch = credit_expenses["Category"].value_counts()
+expen_tot = credit_expenses["Debit"].sum()
+print(f"number of merchandise expenses {sum(merch)} for a total of ${expen_tot}")
+
+### quick plotting pie chart
+plt.figure()
+plt.pie(merch, labels=credit_expenses["Category"].unique(), autopct='%1.1f%%', startangle=90)
+# Title
+plt.title('Expenditure Distribution')
+# Show the plot
+plt.show()
+#
+
+# try to group each type of expenditure and total - histogram
+
+account_tot = RECENT.groupby(["Category"]).sum()
+
+# plt.figure()
+# plt.hist(account_tot["Debit"], bins=8)
+# plt.show()
+
+
+# categories = account_tot['Debit'].value_counts().index
+# counts = account_tot['Debit'].value_counts().values
+# plt.bar(categories, counts, width=0.5)
+
+
+
+
+
+
---
+++ func_load_cap1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 21:48:16 2025
+
+@author: blazer
+"""
+import os 
+import pandas as pd
+import shutil
+
+# attempting to create a function that imports Cap1 data
+# this will focus on returning newest expenses \
+#updating with past database will be done elswhere
+#________________________________________________
+
+
+def load_expense_history(pathname, search_string):
+    
+    working_directoy = os.getcwd()
+    #change to Downloads folder
+    os.chdir(pathname)
+    # Define the string you want to search for in the filename
+    #search_string = 'transaction_download'
+    # Get the current directory
+    current_folder = os.getcwd()
+    # List all files in the current folder that contain 'string' in the filename
+    files_containing_string = []
+    # Iterate through the files in the current folder
+    for filename in os.listdir(current_folder):
+        file_path = os.path.join(current_folder, filename)
+        # Ensure it's a file (and not a directory) and check if the string is in the filename
+        if os.path.isfile(file_path) and search_string in filename:
+            files_containing_string.append(filename)
+    # List the files found
+    print("Files containing 'transaction_download' in the filename:")
+    print(files_containing_string)
+    
+    newest_expenses = []
+    #reading in both bank statements
+    for files in files_containing_string:
+        # print(f"imported {files} statement successfully")
+        df = pd.read_csv(files)
+        newest_expenses.append(df)
+    
+    #if these are transaction downloads recently downloaded from cap1 we want to import and auto move files to archive. 
+    if search_string == "transaction_download":
+        # could be improved, assumes only 2 files (Savor and Venture)
+        full_path = [pathname + "/" + files_containing_string[0], pathname + "/" + files_containing_string[1]]
+        for f in full_path: 
+            shutil.move(f, "/Users/blazer/PyProjects/Personal_Finance/Cap1_Archive")
+    
+    #change back to working directory
+    os.chdir(working_directoy)
+    return(newest_expenses) 
+
+
+# could add in code to remove files from folder 
+# or move them to an arhive folder 
+
+
+#-------old code--------
+
+# # Define the string you want to search for in the filename
+# search_string = 'transaction_download'
+# # Get the current directory
+# current_folder = os.getcwd()
+# # List all files in the current folder that contain 'string' in the filename
+# files_containing_string = []
+# # Iterate through the files in the current folder
+# for filename in os.listdir(current_folder):
+#     file_path = os.path.join(current_folder, filename)
+#     # Ensure it's a file (and not a directory) and check if the string is in the filename
+#     if os.path.isfile(file_path) and search_string in filename:
+#         files_containing_string.append(filename)
+# # List the files found
+# print("Files containing 'transaction_download' in the filename:")
+# print(files_containing_string)
+
+# #initialize dataframe to later append read .csv files
+# dataframes = []
+# #reading in both bank statements
+# for files in files_containing_string:
+#     # print(f"imported {files} statement successfully")
+#     df = pd.read_csv(files)
+#     dataframes.append(df)
+#     # print(df)
+
